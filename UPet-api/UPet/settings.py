8694dd8769ba36cce914ea6ab4f8c464db8245c4
vitorColombiano <vicolombiano69@gmail.com>
2024-10-29 20:35:33 -0300

    discoment database informations

--- UPet-api/UPet/settings.py
+++ UPet-api/UPet/settings.py
@@ -83,16 +83,16 @@ WSGI_APPLICATION = 'UPet.wsgi.application'
 
 load_dotenv()
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST':os.getenv('DB_HOST'),
-#         'PORT':os.getenv('DB_PORT'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST':os.getenv('DB_HOST'),
+        'PORT':os.getenv('DB_PORT'),
+    }
+}
 
 
 
