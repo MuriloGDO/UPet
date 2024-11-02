056b81f69b21ebd5a7579cd858b3930d86efc32f
Murilo Gomes de Oliveira <86263751+MuriloGDO@users.noreply.github.com>
2024-11-02 16:24:19 -0300

    fix: user register service

--- UPet-api/core/services/User_register_service.py
+++ UPet-api/core/services/User_register_service.py
@@ -6,11 +6,6 @@ from ..exceptions import Rollback_exception, User_creation_exception
 class User_register_service:
     @staticmethod
     def create_user(user_data):
-        """
-        Cria um usuário com dados fornecidos e retorna o serializer do usuário.
-        Levanta uma ValidationError em caso de falha.
-        """
-
         if not user_data.get('password') or user_data.get('password').strip() == "":
             raise User_creation_exception("A senha não pode ser vazia.")
         
@@ -25,25 +20,22 @@ class User_register_service:
 
     @staticmethod
     def create_login(user, email, password):
-        """
-        Cria o login para o usuário com base no ID, e-mail e senha fornecidos.
-        Levanta uma Rollback_exception em caso de falha.
-        """
-        login_data = {
-            'user': user.id,
-            'email': email,
-            'password': password
-        }
-        login_serializer = Login_model_serializer(data=login_data)
-        
-        if login_serializer.is_valid():
-            login_serializer.save()
-        else:
-            raise Rollback_exception(login_serializer.errors)
+        try:
+            login_data = {
+                'user': user.id,
+                'institution': None,
+                'email': email,
+                'password': password
+            }
+            login_serializer = Login_model_serializer(data=login_data)
+            
+            if login_serializer.is_valid():
+                login_serializer.save()
+            else:
+                raise Rollback_exception(str(e))
+        except Exception as e:
+            raise Rollback_exception(str(e))
 
     @staticmethod
     def rollback_user(user):
-        """
-        Exclui o usuário em caso de erro ao criar o login.
-        """
         user.delete()
