02a15e1e4bcde97b0d0fb3b8aef234081cfdb1a4
conradogobato <conradogob07@gmail.com>
2024-11-05 16:23:06 -0300

    fix: textbox covered by keyboard

--- UPet-gui/app/_layout.tsx
+++ UPet-gui/app/_layout.tsx
@@ -2,12 +2,19 @@ import { Slot } from 'expo-router';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store'; 
-import { ScrollView } from 'react-native';
+import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 
 export default function HomeLayout() {
   return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1 }}
+    >
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
     <Provider store={store}>
       <Slot />
     </Provider>
+    </ScrollView>
+    </KeyboardAvoidingView>
 );
 }
