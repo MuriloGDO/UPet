3a306f633533aa4a1219c30ec31667ebd0b19bb1
Murilo Gomes de Oliveira <86263751+MuriloGDO@users.noreply.github.com>
2024-11-24 21:16:59 -0300

    fix: pets register view

--- UPet-api/core/api/views/Instituition_view_set.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from ...models import Institution
-from ..serializers import Institution_model_serializer
-
-class Intitution_view_set(ModelViewSet):
-    queryset = Institution.objects.all()
-    serializer_class = Institution_model_serializer
--- UPet-api/core/api/views/Pets_register.py
+++ UPet-api/core/api/views/Pets_register.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from ...models import Pets, Pets_photos, Register_pets
+from ...models import Pets, Pets_photos, Register_pets, Institution
 from ..serializers import Pets_model_serializer, Pets_photos_model_serializer, Register_pets_serializer
 
 class Pets_register(APIView):
@@ -9,6 +9,14 @@ class Pets_register(APIView):
         pet_data = request.data.get('pet')
         if not pet_data:
             return Response({"error": "Pet data is required."}, status=status.HTTP_400_BAD_REQUEST)
+        register_data = request.data.get('register')
+        if not register_data:
+            return Response({"error": "Register data is required."}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            institution_id = register_data.get('institution')
+            institution = Institution.objects.get(id=institution_id)
+        except Institution.DoesNotExist:
+            return Response({"error": f"Institution with ID {institution_id} does not exist."}, status=status.HTTP_404_NOT_FOUND)
         
         pet_serializer = Pets_model_serializer(data=pet_data)
         if pet_serializer.is_valid():
@@ -24,11 +32,6 @@ class Pets_register(APIView):
             else:
                 return Response(photo_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        
-        register_data = request.data.get('register')
-        if not register_data:
-            return Response({"error": "Register data is required."}, status=status.HTTP_400_BAD_REQUEST)
-        
         register_serializer = Register_pets_serializer(data={**register_data, "pet": pet.id})
         if register_serializer.is_valid():
             register_serializer.save()
