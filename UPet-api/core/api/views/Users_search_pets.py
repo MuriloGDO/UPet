bdc2a79a2558594b671a229450f1c675c4f735c8
Murilo Gomes de Oliveira <86263751+MuriloGDO@users.noreply.github.com>
2024-12-15 10:44:51 -0300

    fix: user search pets

--- UPet-api/core/api/views/Users_search_pets.py
+++ UPet-api/core/api/views/Users_search_pets.py
@@ -2,19 +2,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.core.exceptions import ObjectDoesNotExist
-from ...models import Users, Pets
+from ...models import Users, Pets, Pets_photos, Register_pets
 
 class Users_search_pets(APIView):
     def post(self, request):
         user_id = request.data.get('id')
 
         try:
-            # Obtém o usuário pelo ID
             user = Users.objects.get(id=user_id)
 
-            # Obtém os clusters do usuário
-            user_clusters = user.cluster.all()  # Lista de clusters associados ao usuário
-            total_user_clusters = user_clusters.count()  # Número total de clusters do usuário
+            user_clusters = user.cluster.all()  
+            total_user_clusters = user_clusters.count() 
 
             if total_user_clusters == 0:
                 return Response(
@@ -22,43 +20,47 @@ class Users_search_pets(APIView):
                     status=status.HTTP_400_BAD_REQUEST
                 )
 
-            # Obtém todos os pets no banco de dados
             all_pets = Pets.objects.all()
 
-            # Filtra os pets que possuem clusters iguais aos do usuário e calcula a porcentagem
             pets_with_percentage = []
 
             for pet in all_pets:
-                # Calcula a quantidade de clusters do pet que são iguais aos do usuário
                 common_clusters = pet.cluster.filter(id__in=user_clusters.values_list('id', flat=True))
                 common_clusters_count = common_clusters.count()
 
-                # Calcula a porcentagem de clusters compartilhados
                 percentage = (common_clusters_count / total_user_clusters) * 100
 
-                # Adiciona o pet com a porcentagem calculada
                 pets_with_percentage.append({
                     "pet": pet,
                     "percentage": percentage
                 })
 
-            # Ordena os pets pela porcentagem em ordem decrescente
             sorted_pets = sorted(pets_with_percentage, key=lambda x: x["percentage"], reverse=True)
 
-            # Prepara os dados dos pets para resposta
-            pets_data = [
-                {
-                    "id": pet_data["pet"].id,
-                    "name": pet_data["pet"].name,
-                    "date_of_birth": pet_data["pet"].date_of_birth,
-                    "species": pet_data["pet"].species,
-                    "description": pet_data["pet"].description,
-                    "status": pet_data["pet"].status,
-                    "clusters": list(pet_data["pet"].cluster.values('id', 'name')),  # Retorna os clusters do pet
+            pets_data = []
+            for pet_data in sorted_pets:
+                pet = pet_data["pet"]
+                register_pet = Register_pets.objects.filter(pet=pet).first()
+                institution_data = {
+                    "id": register_pet.institution.id,
+                    "name": register_pet.institution.name,
+                    "telephone": register_pet.institution.telephone,
+                    "address": register_pet.institution.address,
+                    "email": register_pet.institution.email
+                } if register_pet else None
+
+                pets_data.append({
+                    "id": pet.id,
+                    "name": pet.name,
+                    "date_of_birth": pet.date_of_birth,
+                    "species": pet.species,
+                    "description": pet.description,
+                    "status": pet.status,
+                    "clusters": list(pet.cluster.values('id', 'name')),  # Retorna os clusters do pet
+                    "institution": institution_data,
+                    "photos": list(Pets_photos.objects.filter(pet=pet).values('id', 'photo', 'uploaded_at')),
                     "percentage": round(pet_data["percentage"], 2)  # Mostra a porcentagem arredondada
-                }
-                for pet_data in sorted_pets
-            ]
+                })
 
             return Response({"matching_pets": pets_data}, status=status.HTTP_200_OK)
 
