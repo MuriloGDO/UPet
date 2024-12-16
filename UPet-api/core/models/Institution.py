bc646966425f497afb5ba4088debdb6674e40026
eduusouza <eduardo.jsouza@hotmail.com>
2024-12-16 12:38:07 -0300

    Feat: Institution information

--- UPet-api/core/models/Institution.py
+++ UPet-api/core/models/Institution.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
 from ..classes import Institution
 
 class Institution(models.Model):
@@ -9,7 +10,9 @@ class Institution(models.Model):
     email = models.EmailField(max_length=100, unique=True)
     password = models.CharField(max_length=128, blank=False, null=False)
 
-    # def find_by_id(id):
-    #     if Institution.objects.filter(id = id).exists():
-    #         institution = Institution.objects.get(id = id)
-    #         return Institution(institution.id, institution.cnpj, ...)
+    def find_by_id(id):
+        if Institution.objects.filter(id = id).exists():
+            institution = Institution.objects.get(id = id)
+            return Institution(institution.id, institution.cnpj, institution.telephone, institution.address, institution.email, institution.password)
+        else:
+            raise ObjectDoesNotExist("Instituição com o ID especificado não foi encontrada.")
