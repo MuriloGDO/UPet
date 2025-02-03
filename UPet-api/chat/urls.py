cfbd3ed7c6d19068e1db293bd84197f1300982ca
Murilo Gomes <gdo.murilo@gmail.com>
2025-02-03 20:34:08 -0300

    fix: url to chat

--- UPet-api/chat/urls.py
+++ UPet-api/chat/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import Message_history
 
 urlpatterns = [
-    path('/history/<str:room_name>/', Message_history.as_view(), name='message-history'),
+    path('history/<str:room_name>/', Message_history.as_view(), name='message-history'),
 ]
