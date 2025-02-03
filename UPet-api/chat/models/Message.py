4ea2de763f1b617ede12041418df4a59a8269a84
Murilo Gomes <gdo.murilo@gmail.com>
2025-02-03 20:33:18 -0300

    fix message model

--- UPet-api/chat/models/Message.py
+++ UPet-api/chat/models/Message.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Message(models.Model):
-    user = models.ForeignKey("core.Users", on_delete=models.CASCADE)
+    user = models.ForeignKey("core.Users", on_delete=models.CASCADE, null=True)
+    institution = models.ForeignKey("core.Institution", on_delete=models.CASCADE, null=True)
     room = models.ForeignKey("chat.Chat_room", on_delete=models.CASCADE, related_name="messages")
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
