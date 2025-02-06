72e876b1c94bbe6feeacf16f5bd78abe43262623
Murilo Gomes de Oliveira <86263751+MuriloGDO@users.noreply.github.com>
2025-02-06 10:19:30 -0300

    feat: chat_rooms

---
+++ UPet-api/chat/views/Chat_rooms.py
@@ -0,0 +1,28 @@
+from rest_framework.generics import ListAPIView
+from ..models import Chat_room
+from ..serializers import Chat_room_serializer
+from rest_framework.filters import SearchFilter
+from rest_framework.response import Response
+
+class Chat_rooms(ListAPIView):
+    serializer_class = Chat_room_serializer
+
+    def get_queryset(self):
+        """
+        Filtra os chats por usuário ou instituição com base nos parâmetros da URL.
+        """
+        queryset = Chat_room.objects.all()
+        user_id = self.request.query_params.get("user_id")
+        institution_id = self.request.query_params.get("institution_id")
+        print(user_id)
+        if user_id != None:
+            user_id = int(user_id)  # Converte para inteiro
+            queryset = queryset.filter(user_id=user_id)
+            return queryset
+
+        if institution_id != None:
+            institution_id = int(institution_id)  # Converte para inteiro
+            queryset = queryset.filter(institution_id=institution_id)
+            return queryset
+
+        return Chat_room.objects.none()
--- UPet-api/chat/views/__init__.py
+++ UPet-api/chat/views/__init__.py
@@ -1,1 +1,3 @@
 from .Message_history import Message_history
+from .Chat_create import Chat_create
+from .Chat_rooms import Chat_rooms
