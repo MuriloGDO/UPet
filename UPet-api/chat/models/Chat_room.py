c619a644f036f33644384214d5ce9f39cc01d799
Murilo Gomes de Oliveira <86263751+MuriloGDO@users.noreply.github.com>
2025-02-06 10:16:53 -0300

    fix chat room

--- UPet-api/chat/models/Chat_room.py
+++ UPet-api/chat/models/Chat_room.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Chat_room(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    user = models.ForeignKey("core.Users", on_delete=models.CASCADE, null=False)
+    institution = models.ForeignKey("core.Institution", on_delete=models.CASCADE, null=False)
 
     def __str__(self):
         return self.name
