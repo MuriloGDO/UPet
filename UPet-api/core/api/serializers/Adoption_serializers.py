2eadc4fa4da46709d2db02509027fb078ce4270d
Murilo Gomes <gdo.murilo@gmail.com>
2025-02-09 13:42:49 -0300

    feat: adoption_serializer

--- UPet-api/core/api/serializers/Adoption_serializers.py
+++ UPet-api/core/api/serializers/Adoption_serializers.py
@@ -1,11 +1,8 @@
 from rest_framework import serializers
 from ...models import Adoption
-from ..serializers import Users_model_serializer, Pets_model_serializer
+from ..serializers import Users_model_serializer, Pets_model_serializer, Institution_model_serializer
 
 class Adoption_model_serializer(serializers.ModelSerializer):
-    user_id = Users_model_serializer(read_only=True)
-    pet_id = Pets_model_serializer(read_only=True)
-
     class Meta:
         model = Adoption
-        fields = ['id', 'user', 'pet', 'date_of_adoption']
+        fields = ['id', 'user', 'pet', 'institution', 'date_of_adoption']
